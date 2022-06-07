9298b7108b7bfbc6a04d5b8ed4cf0e8b43f59d38
youahleum <dkfma6033@naver.com>
2022-06-08 02:22:31 +0900

    입출력과 사칙연산- A+B

---
+++ 1. 입출력과 사칙연산/A+B.js
@@ -0,0 +1,6 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let a = Number(input[0].split(" ")[0]);
+let b = Number(input[0].split(" ")[1]);
+console.log(a + b);
